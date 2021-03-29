4af8fd3414c1d885cd08f7571b9d56263b0e08a7
Nikolay Nakov <nick_70n@yahoo.com>
2021-03-29 14:45:31 +0300

    first commit

---
+++ src/main/java/com/nstoreonline/JenkinsWebappApplication.java
@@ -0,0 +1,13 @@
+package com.nstoreonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nstoreonline/JenkinsWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nstoreonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
